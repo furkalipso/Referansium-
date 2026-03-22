0e55e909112ce0d219925a3595826e80b439d9db
furkalipso <furkannealakaya461@gmail.com>
2026-03-22 18:07:14 +0300

    Update ilanlar.js

--- routes/ilanlar.js
+++ routes/ilanlar.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.json({ basarili: true, mesaj: 'İlanlar endpoint aktif', ilanlar: [] });
+});
+
+module.exports = router;
 
